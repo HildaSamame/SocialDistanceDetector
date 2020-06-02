35f0a3f18392e1feaf8ccf58bb370f5d14f2e5b0
HildaSamame <a20194015@pucp.edu.pe>
2020-06-01 23:01:27 -0500

    Carpeta utils
    Agregar archivos relacionado a utils

---
+++ utils/functions.py
@@ -0,0 +1,207 @@
+
+# -*- coding: utf-8 -*-
+"""
+@author: Hilda Ana Samamé Jimenez
+@code: 20194015
+"""
+import cv2
+import numpy as np
+import pandas as pd
+import math
+from csv import writer
+
+def import_dataset(filename, separator):
+    """
+    Objetivo: guardar los datos el archivo en un dataframe. 
+
+    Parámetros:
+        filename: nombre del archivo
+        separator: tipo de separador utilizado
+        
+    Salida:
+        Retorna el dataframe            
+    """
+    data = pd.read_csv(filename, sep=separator, header=None)
+    data.columns = ["numPersona", "numFrame", "headValid", "bodyValid",
+                "headLeft","headTop","headRight","headBottom",
+                "bodyLeft","bodyTop","bodyRight","bodyBottom"]
+    df = pd.DataFrame(data)
+    
+    return df
+
+def resize_dataframe(df, factor):
+    """
+    Objetivo: escalar los valores de los bounding boxes. 
+
+    Parámetros:
+        df: dataframe con los valores de los bounding boxes
+        factor: escala utilizada. Por ejemplo, si es 2, se escalará a la mitad.
+        
+    Salida:
+        Retorna el dataframe            
+    """    
+    df['bodyLeft'] = df['bodyLeft']//factor
+    df['bodyTop'] = df['bodyTop']//factor
+    df['bodyRight'] = df['bodyRight']//factor
+    df['bodyBottom'] = df['bodyBottom']//factor
+    
+    return df
+
+
+def minimum_distance_pixels(min_distance):
+    """
+    Objetivo: transformar el valor de la mínima distancia en metros a píxels. 
+
+    Parámetros:
+        min_distance: valor de la mínima distancia que debe cumplirse en metros.
+        
+    Salida:
+        Retorna el valor mínimo a cumplir en pixels         
+    """    
+    x = 49.5 * min_distance / 5.51
+    return x
+    
+    
+def eye_bird_view_matrix(filename):
+    """
+    Objetivo: cargar en una variable la matriz de homografía. 
+
+    Parámetros:
+        filename: archivo con los valores de la matriz      
+        
+    Salida:
+        Retorna la matriz         
+    """    
+    return np.loadtxt(open(filename,"rb"),delimiter=",",skiprows=0)
+
+
+def calculate_centroid(px_1, py_1, px_2, py_2):
+    """
+    Objetivo: calcular el centroide de los bounding boxes.
+
+    Parámetros:
+        px_1: abscisa del punto 1
+        py_1: ordenada del punto 1
+        px_2: abscisa del punto 2
+        py_2: ordenada del punto 2
+        
+    Salida:
+        Retorna el frame con borde y título            
+    """
+    cx= int((px_1 + px_2)/2)
+    cy= int((py_1 + py_2)/2)
+    
+    return cx, cy
+
+def transformation_centroid(M, cx, cy):
+    """
+    Objetivo: calcular el centroide de los bounding boxes.
+
+    Parámetros:
+        M: matriz de homografía
+        cx: abscisa del centroide original
+        cy: ordenada del centroide original        
+        
+    Salida:
+        Retorna el centroide transformado por la matriz de homografía. 
+    """
+    arr = np.array([cx,cy,], np.float32)
+    points = arr.reshape(-1,1,2).astype(np.float32)
+    transform_centroid = cv2.perspectiveTransform(points, M)
+
+    return transform_centroid      
+    
+
+def __euclidean_distance(p0, p1):
+    """
+    Objetivo: calcular la distancia euclideana entre dos puntos.
+
+    Parámetros:
+        p0: coordenadas del primer punto
+        p1: coordenadas del segundo punto
+        
+    Salida:
+        Retorna la distancia euclideana.           
+    """
+    return math.sqrt((p0[0] - p1[0])**2 + (p0[1] - p1[1])**2)
+
+
+def get_max_frame(df):
+    """
+    Objetivo: obtener el último frame del dataset.
+
+    Parámetros:
+        df: dataframe de datos
+        
+    Salida:
+        Retorna el valor máximo de los frames.           
+    """
+    max_value = int(df['numFrame'].max())
+    return max_value
+    
+
+def get_lists_transform_centroids(list_transform_centroids, min_distance):
+    """
+    Objetivo: guardar las listas con los valores correspondientes a los que cumplen el distanciamento y los que no cumplen.
+
+    Parámetros:
+        list_transform_centroids: lista con los valores de los centroides después de la homografía.
+        min_distance: mínima distancia (en pixels) para comprobar si se está cumpliendo el distanciamento social.
+    Salida:
+        Retorna la lista de aquellos que cumplen (good_distances) y los que no cumplen (bad_distances).           
+    """
+    good_distances = []
+    bad_distances = []
+    bad_distances_0 = []
+    bad_distances_1 = []
+    
+    for i in range(0, len(list_transform_centroids)-1):
+        v=1
+        for j in range(i+1, len(list_transform_centroids)):            
+            distance = __euclidean_distance(list_transform_centroids[i], list_transform_centroids[j])           
+            if distance < min_distance:
+                bad_distances_0.append(list_transform_centroids[i])
+                bad_distances_1.append(list_transform_centroids[j])
+                bad_distances.append(list_transform_centroids[i])
+                bad_distances.append(list_transform_centroids[j])              
+        good_distances = list(set(tuple(row) for row in list_transform_centroids) - set(tuple(row) for row in bad_distances))
+
+    return good_distances, bad_distances, bad_distances_0, bad_distances_1
+
+
+def filter_df_good_bad(df_frame, good_distances, bad_distances):
+    """
+    Objetivo: crear dos dataframe diferenciando a los que cumplen y los que NO cumplen la distancia mínima.
+
+    Parámetros:
+        df_frame: dataframe con los datos del frame actual.
+        good_distances: lista con los centroides y id de persona que cumplen.
+        bad_distances: lista con los centroides y id de persona que NO cumplen.
+    
+    Salida:
+        Retorna el dataframe de los que cumplen y de los que NO cumplen.
+    """
+
+    good_people = [item[-1] for item in good_distances] 
+    bad_people = [item[-1] for item in bad_distances]
+    df_aux_good = df_frame[df_frame['numPersona'].isin(good_people)]
+    df_aux_bad = df_frame[df_frame['numPersona'].isin(bad_people)]
+
+    return df_aux_good, df_aux_bad
+ 
+
+def get_infractors(aux_infractors, infractors_list):
+    for idPerson in aux_infractors:       
+        if idPerson not in infractors_list:
+            infractors_list.append(idPerson)
+    
+    return infractors_list
+
+def append_list_as_row(file_name, list_of_elem):
+    # Open file in append mode
+    with open(file_name, 'a+', newline='') as write_obj:
+        # Create a writer object from csv module
+        csv_writer = writer(write_obj)
+        # Add contents of list as last row in the csv file
+        csv_writer.writerow(list_of_elem)
+        
---
+++ utils/view.py
@@ -0,0 +1,150 @@
+
+# -*- coding: utf-8 -*-
+"""
+@author: Hilda Ana Samamé Jimenez
+@code: 20194015
+"""
+import cv2
+import numpy as np
+import pandas as pd
+import math
+from csv import writer
+
+
+
+def create_header_title(frame, title, color_header, color_border, logo):
+    """
+    Objetivo: colocar el marco y título al frame. 
+
+    Parámetros:
+        frame: frame a colocar el marco
+        title: título para el frame
+        color_header: color de la cabecera en formato RGB
+        color_border: color del marco en formato RGB
+        
+    Salida:
+        Retorna el frame con borde y título            
+    """
+    
+    font = cv2.FONT_HERSHEY_DUPLEX
+    frame = cv2.copyMakeBorder(frame, 10, 10, 10, 10, cv2.BORDER_CONSTANT, value=color_border)
+    
+    header = np.zeros((80, frame.shape[1], 3), np.uint8)
+    header[:] = color_header
+    
+    if(logo):
+        #cv2.resize(image, (0,0), fx=0.5, fy=0.5) 
+        s_img = cv2.resize(cv2.imread("pucp.jpg"), (0,0), fx=0.2, fy=0.2) 
+        l_img = header
+        x_offset= 800
+        y_offset= 20
+        l_img[y_offset:y_offset+s_img.shape[0], x_offset:x_offset+s_img.shape[1]] = s_img
+        header = l_img    
+    
+    #Concatenar verticalmente el header con el frame
+    vcat = cv2.vconcat((header, frame))
+   
+    cv2.putText(vcat, title, (20,50), font, 1, (0,0,0), 2, 0)
+    
+    return vcat
+
+
+def create_footer(frame, color):
+    font = cv2.FONT_HERSHEY_SIMPLEX
+
+    footer = np.zeros((30, frame.shape[1], 3), np.uint8)
+    footer[:] = color
+   
+    cv2.putText(footer, "CURSO DE COMPUTACION GRAFICA // REALIZADO POR: HILDA SAMAME JIMENEZ ** CODIGO: 20194015", (15,20), font, 0.4, (0,0,0), 1, 0)
+    
+    final = cv2.vconcat((frame, footer))
+    return final
+	
+def map_good_distances(original, result, good_distances, df_aux_good, color, radius, border_rectangle):
+    """
+    Objetivo: mapear en el video original y el Bird's Eye View a los que cumplen la distancia mínima.
+
+    Parámetros:
+        original: frame sobre el cual se colocará los rectángulos.
+        result: frame del bird eye view sobre el cual se colocará los puntos.
+        good_distances: lista con los centroides a mapear
+        df_aux_good: dataframe con los datos de las personas que sí cumplen
+        color: color de los círculos
+        radius: radio de los círculos
+        border_rectangle: grosor del rectángulo de los boxes.
+    """
+    
+    for index, row in df_aux_good.iterrows():
+        x_point1 = int(row['bodyLeft'])
+        y_point1 = int(row['bodyTop'])
+        x_point2 = int(row['bodyRight'])
+        y_point2 = int(row['bodyBottom'])
+        cx = int(row['cx'])
+        cy = int(row['cy'])
+        
+        cv2.circle(original,(cx,cy), 1, color, -1)
+        cv2.rectangle(original,(x_point1,y_point1),(x_point2,y_point2), color, border_rectangle)
+        cv2.putText(original,"Id "+ str(int(row['numPersona'])), (x_point1, y_point1-10), cv2.FONT_HERSHEY_SIMPLEX, 0.4, color, 1)
+       
+        
+    for f in good_distances:
+        cv2.circle(result, tuple(f[:2]), radius, color, -1)
+
+def map_bad_distances(original, result, bad_distances_0, bad_distances_1, df_aux_bad, drawLine, color, radius_circle, border_rectangle):
+    """
+    Objetivo: mapear en el video original y el Bird's Eye View a los que  NO cumplen la distancia mínima.
+
+    Parámetros:
+        original: frame sobre el cual se colocará los rectángulos.
+        result: frame del bird eye view sobre el cual se colocará los puntos.
+        bad_distances: lista con los centroides a mapear
+        df_aux_bad: dataframe con los datos de las personas que NO cumplen
+        color: color de los círculos
+        radius: radio de los círculos
+        border_rectangle: grosor del rectángulo de los boxes.
+    """
+    for index, row in df_aux_bad.iterrows():
+       x_point1 = int(row['bodyLeft'])
+       y_point1 = int(row['bodyTop'])
+       x_point2 = int(row['bodyRight'])
+       y_point2 = int(row['bodyBottom'])       
+       
+       cx = int(row['cx'])
+       cy = int(row['cy'])
+       
+       cv2.circle(original,(cx,cy), 1, color, -1)
+       cv2.rectangle(original,(x_point1,y_point1),(x_point2,y_point2), color, border_rectangle)
+       cv2.putText(original,"Id "+ str(int(row['numPersona'])), (x_point1, y_point1-10), cv2.FONT_HERSHEY_SIMPLEX, 0.4, color, 1)
+       
+    #Uno a unno (de cada lista) se dibuja el centroide y la línea que los une
+    for f, b in zip(bad_distances_0, bad_distances_1):
+        cv2.circle(result, tuple(f[:2]), radius_circle, color, -1)
+        cv2.circle(result, tuple(b[:2]), radius_circle, color, -1)
+        index_i = f[-1]
+        index_j = b[-1]
+        
+        cx_1 = int(df_aux_bad.loc[df_aux_bad['numPersona'] == index_i, 'cx'])
+        cy_1 = int(df_aux_bad.loc[df_aux_bad['numPersona'] == index_i, 'cy'])
+        
+        cx_2 = int(df_aux_bad.loc[df_aux_bad['numPersona'] == index_j, 'cx'])
+        cy_2 = int(df_aux_bad.loc[df_aux_bad['numPersona'] == index_j, 'cy'])
+        
+        if(drawLine):
+            cv2.line(result,tuple(f[:2]) , tuple(b[:2]), color, 2) 
+            cv2.line(original,(cx_1,cy_1) , (cx_2,cy_2), color, 2) 
+
+
+
+def print_text(frame, text, x, y, font_scale, color, thickness):
+    cv2.putText(frame, text, (x,y), cv2.FONT_HERSHEY_SIMPLEX, font_scale, color, thickness)
+
+
+
+def save_video(out, frame, filename):
+    width, height, _ = frame.shape
+    if out is None:
+        fourcc = cv2.VideoWriter_fourcc(*"XVID")
+        out = cv2.VideoWriter(filename, fourcc, 25, (int(height), int(width)))
+    out.write(frame)
+    
+    return out
